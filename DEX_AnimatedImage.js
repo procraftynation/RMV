616f6f867b3128ee1bd80006eb03f126ccd013e6
Procrafty <procraftynation@users.noreply.github.com>
2016-06-15 09:47:11 +0800

    Update DEX_AnimatedImage.js

--- DEX_AnimatedImage.js
+++ DEX_AnimatedImage.js
@@ -1,6 +1,6 @@
 /*:
  * @plugindesc v1.00 Show an image with animation
- * @author Procraftynation - https://github.com/procraftynation
+ * @author Procraftynation - https://www.youtube.com/c/procraftynation
  * =====================================================================
  * Usage
  * =====================================================================
@@ -73,7 +73,7 @@
  * Terms of Use
  * =====================================================================
  * Free for use in non-commercial and commercial games just give credit
- * and link back to https://github.com/procraftynation/RMV.
+ * and link back to https://www.youtube.com/c/procraftynation.
  * If commercial, a free copy of the game is awesome but optional.
  * =====================================================================*/
 
